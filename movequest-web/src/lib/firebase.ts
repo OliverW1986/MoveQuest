57a25be524af5a4d6e763b7b70148941c78d4f5a
Oliver Weller <ImpactTang@outlook.com>
2025-12-04 00:11:20 -0600

    Setup initial website structure-fixed api key

--- movequest-web/src/lib/firebase.ts
+++ movequest-web/src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "movequest-dev.firebaseapp.com",
-  projectId: "movequest-dev",
-  storageBucket: "movequest-dev.firebasestorage.app",
-  messagingSenderId: "669397140848",
-  appId: "1:669397140848:web:961a5e9844f4d48b833bb5",
-  measurementId: "G-Y3S2LYDKMP"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!
 };
 
 const app = initializeApp(firebaseConfig);
